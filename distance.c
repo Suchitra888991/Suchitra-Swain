67816c8a91d8a08ce4ced23af042d5ae8c8370d5
Suchitra Swain <69208308+Suchitra888991@users.noreply.github.com>
2021-02-06 20:56:54 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -20,10 +20,10 @@ int result(int x1, int y1, int x2, int y2)
 }
 int main()
 {
-   printf(“Enter x1 and x2: ”);
+   printf(“Enter x1 and y1: ”);
    int x1=input();
    int y1=input();
-   printf(“Enter y1 and y2: ”);
+   printf(“Enter x2 and y2: ”);
    int x2=input();
    int y2=input();
    result(x1,y1,x2,y2);
