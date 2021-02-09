81e0fd1c26d0cafc58d41364588a553b10710f5b
Suchitra Swain <69208308+Suchitra888991@users.noreply.github.com>
2021-02-09 20:56:31 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,31 +1,27 @@
 #include<stdio.h>
 #include<math.h>
-float point1()
+void input()
 {
-   float x;
-   scanf("%f",&x);
-   return x;
+    int x1, y1, x2, y2;
+    printf("Enter point 1: ");
+    scanf("%d%d", &x1&y1);
+    printf("Enter point 2: ");
+    scanf("%d%d", &x2,&y2);
+    output(x1, y1, x2, y2);
 }
-float point2()
+float distance(int x1, int y1, int x2, int y2)
 {
-   float y;
-   scanf("%f",&y);
-   return y;
+ float dist = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+ return dist;
 }
-float distance(float x1, float y1, float x2, float y2)
+
+void output(int x1, int y1, int x2, int y2)
 {
-    
-    float d=sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-    return d;
+ printf("The distance between %d, %d and %d, %d is %f.\n", x1, y1, x2, y2, distance(x1,y1,x2,y2));
 }
+
 int main()
 {
-   printf("Enter x1 and y1: ");
-   float x1=point1();
-   float y1=point2();
-   printf("Enter x2 and y2: ");
-   float x2=point1();
-   float y2=point2();
-   printf("The distance between %f, %f and %f, %f is %f",x1,y1,x2,y2,distance(x1,y1,x2,y2));
-   return 0;
+ input();
+ return 0;
 }
